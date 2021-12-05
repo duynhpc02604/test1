43d8ff480a1ac79dcc7a105052608bb76014fb3d
duynhpc02604 <duynhpc02604@fpt.edu.vn>
2021-12-06 02:14:17 +0700

    lam lai layout va responsive nhung gi da co

--- layout-khach.php
+++ layout-khach.php
@@ -10,63 +10,96 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <!-- My link -->
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
   <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/style.css">
   <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/slide.css">
 </head>
 
 <body>
-<!-- Header -->
-<header>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+  <!-- Header -->
+  <header>
+    <div class="container-fluid">
+      <!-- row top bar -->
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12" style="padding: 0;">
           <div class="top_bar">
-            Xây dựng cấu hình <span class="hight_light">PC Gaming - Workstation</span>
-          </div>
-      </div>    
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <div class="top_bar2">
-          <div class="top_bar2_link">
-            <a href="">Hệ thống Showroom</a>
-            <a href="">Bán hàng trực tuyến</a>
-            <a href="">Bán trả góp</a>
-            <a href="">Khuyến mãi</a>
-            <a href="">Tin tức</a>
+            Xây dựng cấu hình <span class="high_light">PC Gaming - Workstation</span>
           </div>
         </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <div class="main_header">
-          <a class="link-index" href="index.php"><img src="/test1/content/images/logo-final.png" alt="logo-final" class="logo"></a>
-          <div class="search">
-            <form action="#" method="GET">
-              <input type="text" class="search-input" placeholder="Bạn tìm gì...">
-              <button type="submit"><i class="fas fa-search"></i></button>
-            </form>
-          </div>
-          <div class="hide_for_medium_scr">
-            <div class="hotline">
-              <span>Mua hàng online</span><br>
-              <a href="">19001580</a>
-            </div>
-            <div class="login">
-              <a href="">Đăng nhập</a>&nbsp;/&nbsp; <a href="">Đăng ký</a>
-            </div>
-            <div class="cart">
-              <a href=""><i class="fas fa-shopping-cart"></i> Giỏ hàng</a>
+      <!-- end row top bar -->
+
+      <!-- row top bar2 -->
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12" style="padding: 0;">
+          <div class="top_bar2">
+            <div class="top_bar2_link_group">
+              <a class="top_bar2_link" href="">Hệ thống Showroom</a>
+              <a class="top_bar2_link" href="">Bán hàng trực tuyến</a>
+              <a class="top_bar2_link" href="">Bán trả góp</a>
+              <a class="top_bar2_link" href="">Khuyến mãi</a>
+              <a class="top_bar2_link" href="">Tin tức</a>
             </div>
           </div>
         </div>
       </div>
+      <!-- end row top bar2 -->
+
+      <!-- row main header -->
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <div class="main_header">
+            <!-- col logo -->
+            <div class="col-sm-2 col-md-2 col-lg-2 col-xl-2 hide_for_small_scr">
+              <a class="link-index" href="index.php">
+                <img src="/test1/content/images/logo-final.png" alt="logo-final" class="logo">
+              </a>
+            </div>
+            <!-- col logo -->
+
+            <!-- col search -->
+            <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4">
+              <div class="search">
+                <form action="#" method="GET">
+                  <input type="text" class="search-input" placeholder="Bạn tìm gì...">
+                  <button type="submit"><i class="fas fa-search"></i></button>
+                </form>
+              </div>
+            </div>
+            <!-- col search -->
+
+            <!-- col hide for small screen -->
+              <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6 hide_for_small_scr">
+                <div class="hide_for_medium_scr">
+                  <div class="hotline hide_md_scr">
+                    <span>Mua hàng online</span><br>
+                    <a href="">19001580</a>
+                  </div>
+                  <div class="login">
+                    <a href="">Đăng nhập</a>&nbsp;/&nbsp; <a href="">Đăng ký</a>
+                  </div>
+                  <div class="cart">
+                    <a href=""><i class="fas fa-shopping-cart"></i> Giỏ hàng</a>
+                  </div>
+                </div>
+              </div>
+            <!-- col hide for small screen -->
+          </div>
+        </div>
+        <!-- end row main header -->
+        <!-- row nav -->
+        <div class="row">
+          <div class="row_nav">
+            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end row nav -->
     </div>
-  </div>
-</header>
-<!-- End Header -->
+  </header>
+  <!-- End Header -->
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
